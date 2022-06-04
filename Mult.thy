23d767fbf9714abefd7b9cbc018c1dba9f956adc
jean-lucca <jean.lucca.uchaki@gmail.com>
2022-06-04 17:06:26 -0300

    definicao recursiva feita

--- Mult.thy
+++ Mult.thy
@@ -1,11 +1,22 @@
 theory Mult
-imports Main
+  imports Main Add
 begin
 
-(*funcao recursiva de multiplicar  nat*)
+(*  mult: \<nat> \<times> \<nat> \<rightarrow> \<nat>  *)
+(*  requer \<top>  *)
+(*  garante mult(x, y) = x \<^emph> y  *)
+(*  mult (x, y) = 0, se y = 0  *)
+(*  mult (x, y) = x + mult (x, y − 1), se y > 0  *)
+
 primrec mult::"nat \<Rightarrow> nat \<Rightarrow> nat"
 where
-  mult01: "mult x 0 = x" |
-  mult02: "mult x (Suc y) = Suc (mult x y)"
+  mult01: "mult x 0 = 0"|
+  mult02: "mult x (Suc y) = add x (mult x y)"
+
+value "mult 2 2"
+value "mult 1 2"
+value "mult 2 0"
+value "mult 0 1"
+
 end
 
---
+++ Mult.thy~
@@ -0,0 +1,11 @@
+theory Mult
+imports Main
+begin
+
+(*funcao recursiva de multiplicar  nat*)
+primrec mult::"nat \<Rightarrow> nat \<Rightarrow> nat"
+where
+  mult01: "mult x 0 = x" |
+  mult02: "mult x (Suc y) = Suc (mult x y)"
+end
+
